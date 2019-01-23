4e91f78980b5921dda1a83e1633a2421a65273b8
Danilo de Febba Santos <danilo.de.febba.santos@gmail.com>
2019-01-22 23:07:42 -0200

    construção

--- dmCalendar.sql
+++ dmCalendar.sql
@@ -14,37 +14,36 @@ GO
 
 CREATE TABLE [DW].[dmCale] (
     [SurrKeyDate]     integer     NOT NULL,          -- Surrogate Key
-    [Ano]             smallint    NOT NULL,          -- Ano
-    [NumeSeme]        tinyint     NOT NULL,          -- Número do Semestre
-    [NomeCompSeme]    varchar(11) NOT NULL,          -- Nome Completo do Semestre
-    [NomeAbreSeme]    varchar(2)  NOT NULL,          -- Nome Abreviado do Semestre
-
-    [NumeTrim]        tinyint     NOT NULL,          -- Campo Número do Trimestre
-    [NomeTrim]        varchar(12) NOT NULL,          -- Campo Nome do Trimestre
-    [NomeTrimAbre]    varchar(2)  NOT NULL,          -- Campo Nome do Trimestre Abreviado
-    [NumeMes]         tinyint     NOT NULL,          -- Campo Número do Mês
-    [NomeMes]         varchar(8)  NOT NULL,          -- Campo Nome do Mês
-    [NomeMesAbre]     varchar(3)  NOT NULL,          -- Campo Nome do Mês Abreviado
-    [UltiDiaMes]      tinyint     NOT NULL,          -- Campo Último Dia do Mês
-    [NumeQuin]        tinyint     NOT NULL,          -- Campo Número da Quianzena
-    [NomeQuin]        varchar(11) NOT NULL,          -- Campo Nome da Quianzena
-    [NumeSemaAno]     tinyint     NOT NULL,          -- Campo Número da Semana do Ano ISO 8601
-    [NomeSemaAno]     varchar(33) NOT NULL,          -- Campo Nome da Semana do Ano ISO 8601
-    [NomeSemaAnoAbre] varchar(8)  NOT NULL,          -- Campo Nome da Semana do Ano Abreviado ISO 8601
-    [NumeDiaSema]     tinyint     NOT NULL,          -- Campo Número do Dia da Semana
-    [NomeDiaSema]     varchar(13) NOT NULL,          -- Campo Nome do Dia da Semana
-    [NomeDiaSemaAbre] varchar(3)  NOT NULL,          -- Campo Nome do Dia da Semana Abreviado
-    [Data]            date        NOT NULL,          -- Data
-    [UnixEpoc]        integer     NOT NULL,          -- Campo Posix Time ou Unix Epoch ou Unix Timestamp
-    [Dia]             tinyint     NOT NULL,          -- Campo Dia
-    [NumeDiaAno]      smallint    NOT NULL,          -- Campo Número do Dia do Ano
+    [Ano]             smallint        NULL,          -- Ano
+    [NumeSeme]        tinyint         NULL,          -- Número do Semestre
+    [NomeCompSeme]    varchar(11)     NULL,          -- Nome Completo do Semestre
+    [NomeAbreSeme]    varchar(2)      NULL,          -- Nome Abreviado do Semestre
+    [NumeTrim]        tinyint         NULL,          -- Número do Trimestre
+    [NomeCompTrim]    varchar(12)     NULL,          -- Nome Completo do Trimestre
+    [NomeAbreTrim]    varchar(2)      NULL,          -- Nome Abreviado do Trimestre
+    [NumeMes]         tinyint         NULL,          -- Número do Mês
+    [NomeCompMes]     varchar(8)      NULL,          -- Nome Completo do Mês
+    [NomeAbreMes]     varchar(3)      NULL,          -- Nome Abreviado do Mês
+    [UltiDiaMes]      tinyint         NULL,          -- Último Dia do Mês
+    [NumeQuin]        tinyint         NULL,          -- Número da Quianzena
+    [NomeQuin]        varchar(11)     NULL,          -- Nome da Quianzena
+    [NumeSemaAno]     tinyint         NULL,          -- Número da Semana do Ano (ISO 8601)
+    [NomeCompSemaAno] varchar(33)     NULL,          -- Nome Completo da Semana do Ano (ISO 8601)
+    [NomeAbreSemaAno] varchar(8)      NULL,          -- Nome Abreviado da Semana do Ano (ISO 8601)
+    [NumeDiaSema]     tinyint         NULL,          -- Número do Dia da Semana
+    [NomeCompDiaSema] varchar(13)     NULL,          -- Nome Completo do Dia da Semana
+    [NomeAbreDiaSema] varchar(3)      NULL,          -- Nome Abreviado do Dia da Semana
+    [Data]            date            NULL,          -- Data
+    [UnixEpoc]        integer         NULL,          -- Posix Time ou Unix Epoch ou Unix Timestamp
+    [Dia]             tinyint         NULL,          -- Dia
+    [NumeDiaAno]      smallint        NULL,          -- Número do Dia do Ano
 );
 -- Primary Key Constraint of the Calendar Dimension Table
 ALTER TABLE [DW].[dmCale] ADD CONSTRAINT [pkDimeCale] PRIMARY KEY ([SurrKeyDate]);
 GO
 
-DECLARE @DataInicio date = '2016-12-26' --CONVERT(varchar(4), YEAR(GETDATE())) + '-01-01';
-DECLARE @DataFim date = '2026-01-04' --DATEADD(DAY, -1, DATEADD(YEAR, 10, @DataInicio));
+DECLARE @DataInicio date = CONVERT(varchar(4), YEAR(GETDATE())) + '-01-01';
+DECLARE @DataFim date = DATEADD(DAY, -1, DATEADD(YEAR, 1, @DataInicio));
 DECLARE @Data date = @DataInicio;
 
 SET LANGUAGE portuguese;
@@ -58,20 +57,20 @@ BEGIN
              , [NumeSeme]     = CONVERT(tinyint, CASE WHEN MONTH(@Data) < 7 THEN 1 ELSE 2 END)
              , [NomeCompSeme] = CONVERT(varchar(11), CASE WHEN MONTH(@Data) < 7 THEN '1º semestre' ELSE '2º semestre' END)
              , [NomeAbreSeme] = CONVERT(varchar(2), CASE WHEN MONTH(@Data) < 7 THEN 'S1' ELSE 'S2' END)
-             , CONVERT(tinyint, DATEPART(QUARTER, @Data))                                                                        AS [NumeTrim]        -- Número do Trimestre
-             , CONVERT(varchar(12), CASE DATEPART(QUARTER, @Data)
-                                        WHEN 1 THEN '1º trimestre'
-                                        WHEN 2 THEN '2º trimestre'
-                                        WHEN 3 THEN '3º trimestre'
-                                        ELSE '4º trimestre'
-                                    END)                                                                                         AS [NomeTrim]        -- Nome do Trimestre
-             , CONVERT(varchar(2), 'T' + CONVERT(varchar(1), DATEPART(QUARTER, @Data)))                                          AS [NomeTrimAbre]    -- Nome do Trimestre Abreviado
-             , CONVERT(tinyint, MONTH(@Data))                                                                                    AS [NumeMes]         -- Número do Mês
-             , CONVERT(varchar(8), DATENAME(MONTH, @Data))                                                                       AS [NomeMes]         -- Nome do Mês
-             , CONVERT(varchar(3), DATENAME(MONTH, @Data))                                                                       AS [NomeMesAbre]     -- Nome do Mês Abreviado
-             , CONVERT(tinyint, DAY(EOMONTH(@Data)))                                                                             AS [UltiDiaMes]      -- Último Dia do Mês
-             , CONVERT(tinyint, CASE WHEN DAY(@Data) < 16 THEN 1 ELSE 2 END)                                                     AS [NumeQuin]        -- Número da Quianzena
-             , CONVERT(varchar(11), CASE WHEN DAY(@Data) < 16 THEN '1º quinzena' ELSE '2º quinzena' END)                         AS [NomeQuin]        -- Nome da Quianzena
+             , [NumeTrim]     = CONVERT(tinyint, DATEPART(QUARTER, @Data))
+             , [NomeCompTrim] = CONVERT(varchar(12), CASE DATEPART(QUARTER, @Data)
+                                                         WHEN 1 THEN '1º trimestre'
+                                                         WHEN 2 THEN '2º trimestre'
+                                                         WHEN 3 THEN '3º trimestre'
+                                                         ELSE '4º trimestre'
+                                                     END)
+             , [NomeAbreTrim] = CONVERT(varchar(2), 'T' + CONVERT(varchar(1), DATEPART(QUARTER, @Data)))
+             , [NumeMes]      = CONVERT(tinyint, MONTH(@Data))
+             , [NomeCompMes]  = CONVERT(varchar(8), DATENAME(MONTH, @Data))
+             , [NomeAbreMes]  = CONVERT(varchar(3), DATENAME(MONTH, @Data))
+             , [UltiDiaMes]   = CONVERT(tinyint, DAY(EOMONTH(@Data)))
+             , [NumeQuin]     = CONVERT(tinyint, CASE WHEN DAY(@Data) < 16 THEN 1 ELSE 2 END)
+             , [NomeQuin]     = CONVERT(varchar(11), CASE WHEN DAY(@Data) < 16 THEN '1º quinzena' ELSE '2º quinzena' END)
              , CONVERT(tinyint, DATEPART(ISO_WEEK, @Data))                                                                       AS [NumeSemaAno]     -- Número da Semana do Ano ISO 8601
              , CONVERT(varchar(33), CASE WHEN DATEPART(ISO_WEEK, @Data) = 1 THEN
                                         CONVERT(varchar(4), YEAR(DATEADD(DAY, 7 - DATEPART(WEEKDAY, @Data), @Data))) + '-' + 
