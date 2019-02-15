1f659c23922a1d7e8b2fc7856f698557fb2ed0cd
Danilo de Febba Santos <danilo.de.febba.santos@gmail.com>
2019-02-15 00:27:14 -0200

    Desenvolvimento

--- Dimensions/dimTime.sql
+++ Dimensions/dimTime.sql
@@ -1,2 +1,3 @@
-SELECT "skTime"
-  FROM generate_series(0,84600,1800) AS "skTime";
+
+SELECT "nkTime"
+  FROM generate_series('00:00:00'::time without time zone,'23:59:59'::time without time zone,interval '30 minute') AS "nkTime";
